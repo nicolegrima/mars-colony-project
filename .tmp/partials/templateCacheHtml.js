494fa0c5d029a0addd40be9db3fbae6d6f06ec38
Nicole Grima <nicolegrima@Nicoles-MacBook-Air.local>
2016-02-24 11:41:24 -0800

    Add in GET and POST formats

--- .tmp/partials/templateCacheHtml.js
+++ .tmp/partials/templateCacheHtml.js
@@ -1,5 +1,5 @@
 angular.module("red").run(["$templateCache", function($templateCache) {$templateCache.put("app/Report/report-filed.html","<h1>Report-Filed TEST</h1>");
-$templateCache.put("app/Report/report.html","<h1>Report TEST</h1>");
-$templateCache.put("app/check-in/check-in.html","<div class=\"checkin-wrapper\"><h1>Check-in TEST</h1></div>");
+$templateCache.put("app/Report/report.html","<div class=\"report-wrapper\"><h2>Report Encounter</h2><h3>Safety on Mars is your responsibility</h3><button>Submit Report</button></div>");
+$templateCache.put("app/check-in/check-in.html","<div class=\"checkin-wrapper\"><h2>Register</h2><button>Check in</button></div>");
 $templateCache.put("app/encounters/encounters.html","<div class=\"encounters-wrapper\"><h2>Recent Encounters</h2><h3>See an Alien? Report it!</h3><button>Report Encounter</button></div>");
-$templateCache.put("app/main/main.html","<div class=\"welcome\"><h1>{{ description }}</h1></div>");}]);
+$templateCache.put("app/main/main.html","<div class=\"main-wrapper\"><h2>Mars Colony 1</h2><h3>Tap circle to enter</h3></div>");}]);
--- src/app/check-in/CheckinCtrl.js
+++ src/app/check-in/CheckinCtrl.js
@@ -6,7 +6,40 @@
     .controller('CheckinCtrl', CheckinCtrl);
 
   /** @ngInject */
-  function CheckinCtrl($scope) {
+  function CheckinCtrl($scope, $http) {
+  	var JOBS_GET_URL = 'https://red-wdp-api.herokuapp.com/api/mars/jobs';
+  	var COLONIST_POST_URL = 'https://red-wdp-api.herokuapp.com/api/mars/colonists';
+
+// placeholder for POST request to /colonist
+  	$scope.colonist = {};
+
+// fetch all the jobs 
+  	$http({
+  		method: 'GET',
+  		url: JOBS_GET_URL
+  	}).then(function(response) {
+  			$scope.jobs = response.data.jobs;
+  	}, function(error) {
+  			// TO DO for Back END: Handle Error 
+  	});
+
+
+  	$scope.login = function() {
+  		event.preventDefault();
+
+  		$http({
+  			method: 'POST',
+  			url: COLONIST_POST_URL,
+  			data: {
+				"colonist" : $scope.colonist
+			}
+
+  		}).then(function(response) {
+  			console.log(response);
+  		}, function(error){
+  			console.log(error);
+  		});
+  	}
 
   }
 
