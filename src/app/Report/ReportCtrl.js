2499cc508d34a425e0d8ec2397c472406e446b71
Nicole Grima <nicolegrima@Nicoles-MacBook-Air.local>
2016-02-28 13:33:50 -0800

    Add Report Style and API requests

--- src/app/Report/ReportCtrl.js
+++ src/app/Report/ReportCtrl.js
@@ -1,50 +1,52 @@
-// (function() {
-//   'use strict';
+(function() {
+  'use strict';
+
+  angular
+    .module('red')
+    .controller('ReportCtrl', ReportCtrl);
+
+  /** @ngInject */
+  function ReportCtrl($scope, $cookies, $http, $state, $filter) {
+    var ENCOUNTER_POST_URL = 'https://red-wdp-api.herokuapp.com/api/mars/reports';
+    var ALIENS_GET_URL = 'https://red-wdp-api.herokuapp.com/api/mars/aliens';
+
+    $https({
+      method: 'GET',
+      url: ALIENS_GET_URL
+    }).then(function(response) {
+          $scope.aliens = response.data.aliens;
+    }, function(error) {
+      // TO DO for Back-End Dev: Handle Error
+  });
+
+        $scope.showValidation = false;
+
+        $scope.encounter = {
+          colonist_id: $cookies.getObject('mars_colonist').id,
+          date: $filter('date')(newDate(), 'yyyy-MM-dd')
+        };
+
+    $scope.submitReport = function(e, form) {
+        e.preventDefault();
+
+        if (form.$invalid) {
+            $scope.showValidation = true;
+          } else {
+
+    $http({
+      method: 'POST',
+      url: ENCOUNTER_POST_URL,
+      data: {
+        "encounter" : $scope.encounter
+      }
+
+    }).then(function(response) {
+              $state.go('encounters');
+    }, function(error) {
+          // TO DO for Back-End Dev: Handle Error
+    });
 //
-//   angular
-//     .module('red')
-//     .controller('ReportCtrl', ReportCtrl);
-//
-//   /** @ngInject */
-//   function ReportCtrl($scope, $cookies, $http, $state, $filter) {
-//     var ENCOUNTER_POST_URL = 'https://red-wdp-api.herokuapp.com/api/mars/reports';
-//     var ALIENS_GET_URL = 'https://red-wdp-api.herokuapp.com/api/mars/aliens';
-//
-//     $scope.showValidation = false;
-//
-//     $scope.encounter = {
-//       colonist_id: $cookies.getObject('mars_colonist').id,
-//       date: $filter('date')(newDate(), 'yyyy-MM-dd')
-//     };
-//
-//     $https({
-//       method: 'GET',
-//       url: ALIENS_GET_URL
-//     }).then(function(response) {
-//           $scope.aliens = response.data.aliens;
-//     }), function(error) {
-//       // placeholder
-//     });
-//
-//     $scope.submitReport = function(e, form) {
-//         e.preventDefault();
-//
-//         if (form.$invalid) {
-//             $scope.showValidation = true;
-//           } else {
-//
-//     $http({
-//       method: 'POST',
-//       url: ENCOUNTER_POST_URL,
-//       data: {
-//         "encounter" : $scope.encounter
-//       }
-//
-//     }).then(function(response) {
-//           // console.log(response);
-//     }), function(error) {
-//           // console.log(error);
-//     };
-//   })
-// }
-// })();
+        }
+      };
+   }
+})();
